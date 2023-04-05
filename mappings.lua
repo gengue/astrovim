5cbf5613c541c6cd0f4501b9743c59115d5b7f7b
Genesis Guerrero Martinez <genesisdaft@gmail.com>
2023-04-05 10:57:23 +0200

    leave old setup mappings for buffers

--- mappings.lua
+++ mappings.lua
@@ -11,13 +11,33 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    -- ["<leader>l"] = {
+    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+    --   desc = "Next buffer",
+    -- },
+    -- ["<leader>h"] = {
+    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+    --   desc = "Previous buffer",
+    -- },
+    -- ["<leader>H"] = {
+    --   function()
+    --     local wins = vim.api.nvim_tabpage_list_wins(0)
+    --     if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
+    --       vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
+    --     end
+    --     require("alpha").start(false, require("alpha").default_config)
+    --   end,
+    --   desc = "Home Screen",
+    -- },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
